84eeccc48110a4cf4c06f6a152ffc4fef915387c
Victoric3 <120360885+Victoric3@users.noreply.github.com>
2024-06-18 07:08:24 +0100

    Update tokenHelpers.js

--- Helpers/auth/tokenHelpers.js
+++ Helpers/auth/tokenHelpers.js
@@ -15,17 +15,28 @@ const getAccessTokenFromHeader = (req) => {
     return access_token
 }
 
-const sendToken = (user,statusCode ,res, message)=>{
+const sendToken = (user, statusCode, res, message) => {
+    const token = user.generateJwtFromUser();
 
-    const token = user.generateJwtFromUser()
+    // Set cookie options
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+        ), // Cookie expiry set to the value in the environment variable
+        httpOnly: process.env.NODE_ENV === 'production', // Cookie cannot be accessed through client-side scripts
+        secure: process.env.NODE_ENV === 'production' // Send cookie only over HTTPS in production
+    };
 
+    // Set the token as a cookie
+    res.cookie('token', token, cookieOptions);
+
+    // Send the response
     return res.status(statusCode).json({
         status: 'success',
-        message,
-        token
-    })
-
+        message
+    });
 }
+
 
 module.exports ={
     sendToken,
